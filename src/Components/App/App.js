4b8e8670c4f54152bbb890362dfa4d6ba41fd075
[almccloskey@me.com] <60784483+almccloskey@users.noreply.github.com>
2021-05-03 18:28:48 -0500

    setting Search Result state and passing to component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,6 +6,15 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [{name: '', artis: '', album: '', id: 1}, 
+      {name: '', artis: '', album: '', id: 2}, 
+      {name: '', artis: '', album: '', id: 3}]
+    };
+  }
+  
   render() {
     return (
       <div>
@@ -13,7 +22,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
+            <SearchResults searchResults={this.state.searchResults}/>
             <Playlist />
           </div>
         </div>
