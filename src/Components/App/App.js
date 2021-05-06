c4c895994ba8aec3f753c17ec6a84c54a69f333c
[almccloskey@me.com] <60784483+almccloskey@users.noreply.github.com>
2021-05-06 18:06:26 -0500

    Passing playlist props down

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,6 +11,10 @@ class App extends React.Component {
     this.state = {
       searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1}, 
       {name: 'name2', artist: 'artist2', album: 'album2', id: 2}, 
+      {name: 'name3', artist: 'artist3', album: 'album3', id: 3}],
+      playlistName: 'That New New',
+      playlistTracks: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1},
+      {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
       {name: 'name3', artist: 'artist3', album: 'album3', id: 3}]
     };
   }
@@ -22,8 +26,9 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults}/>
-            <Playlist />
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist playlistName={this.state.playlistName} 
+              playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
       </div>
